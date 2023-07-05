664302c3be044122cc79e69ce578d6de873407e1
Ruslan Akhmadiev <ahmadievrr@gmail.com>
2023-07-05 06:37:45 +0300

    env added

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps : [{
+    name   : "app1",
+    script : "./app.js"
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'git@github.com:Qolit3/Deploy-front.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+}
